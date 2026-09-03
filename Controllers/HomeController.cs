2f0d35abab5068744396cabb239dc8d780c32412
yousef abdelwahab <yousefmahmoad@gmail.com>
2026-09-03 10:15:24 +0300

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace RealEstateSystem.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+       
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
